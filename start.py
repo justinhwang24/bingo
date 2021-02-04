430c8bc7b5e8a350f13f24f8de409df11065d0ab
Justin Hwang <advancedjus@gmail.com>
2021-02-04 09:13:18 -0500

    Created button

--- start.py
+++ start.py
@@ -1,25 +1,72 @@
-# Import the pygame module
-import pygame
+import pygame 
+import sys 
+  
+  
+# initializing the constructor 
+pygame.init() 
+  
+# screen resolution 
+res = (720,720) 
+  
+# opens up a window 
+screen = pygame.display.set_mode(res) 
+  
+# white color 
+color = (255,255,255) 
+  
+# light shade of the button 
+color_light = (0, 160, 181) 
+  
+# dark shade of the button 
+color_dark = (0, 79, 89) 
+  
+# stores the width of the 
+# screen into a variable 
+width = screen.get_width() 
+  
+# stores the height of the 
+# screen into a variable 
+height = screen.get_height() 
+  
+# defining a font 
+smallfont = pygame.font.SysFont('Helvetica',35) 
+  
+# rendering a text written in 
+# this font 
+text = smallfont.render('Start' , True , color) 
+  
+while True: 
+    # fills the screen with a color 
+    screen.fill((3, 152, 252)) 
+      
+    # stores the (x,y) coordinates into 
+    # the variable as a tuple 
+    mouse = pygame.mouse.get_pos()
 
-# Import pygame.locals for easier access to key coordinates
-# Updated to conform to flake8 and black standards
-from pygame.locals import (
-    K_UP,
-    K_DOWN,
-    K_LEFT,
-    K_RIGHT,
-    K_ESCAPE,
-    KEYDOWN,
-    QUIT,
-)
-
-# Initialize pygame
-pygame.init()
-
-# Define constants for the screen width and height
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-
-# Create the screen object
-# The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    for ev in pygame.event.get(): 
+        
+        if ev.type == pygame.QUIT: 
+            pygame.quit() 
+              
+        #checks if a mouse is clicked 
+        if ev.type == pygame.MOUSEBUTTONDOWN: 
+              
+            #if the mouse is clicked on the 
+            # button the game is terminated 
+            if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40: 
+                pygame.quit()
+                # TODO - START
+      
+    # if mouse is hovered on a button it 
+    # changes to lighter shade  
+    if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40: 
+        pygame.draw.rect(screen,color_light,[width/2,height/2,140,40]) 
+          
+    else: 
+        pygame.draw.rect(screen,color_dark,[width/2,height/2,140,40]) 
+      
+    # superimposing the text onto our button 
+    screen.blit(text , (width/2 + 50, height/2)) 
+      
+    # updates the frames of the game 
+    pygame.display.update()
