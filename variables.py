9f0a0ffa3c297ce08201a8307021ad3194979bc4
Justin Hwang <advancedjus@gmail.com>
2021-03-04 18:18:30 -0500

    Created file

---
+++ variables.py
@@ -0,0 +1,24 @@
+WHITE = (230, 230, 230)
+RED = (217, 35, 22)
+ORANGE = (217, 129, 22)
+YELLOW = (252, 206, 3)
+GREEN = (94, 217, 22)
+BLUE = (3, 152, 252) 
+DARK_BLUE = (22, 61, 217)
+PURPLE = (132, 22, 217)
+PINK = (217, 22, 201)
+BLACK = (59, 59, 59)
+
+colors = []
+colors.append(WHITE)
+colors.append(RED)
+colors.append(ORANGE)
+colors.append(YELLOW)
+colors.append(GREEN)
+colors.append(BLUE)
+colors.append(DARK_BLUE)
+colors.append(PURPLE)
+colors.append(PINK)
+colors.append(BLACK)
+
+palette = [BLUE, BLUE]
